b8ae3eb4ace8646828f95e38a860e4f7eb1de20d
David Poindexter <dpoindexter@nvisionative.com>
2019-07-14 23:56:00 -0400

    Style the color mode switcher

--- DNN Platform/Dnn.Html.CustomElements/CustomElements.Web/src/components/dnn-color-picker/dnn-color-picker.tsx
+++ DNN Platform/Dnn.Html.CustomElements/CustomElements.Web/src/components/dnn-color-picker/dnn-color-picker.tsx
@@ -199,10 +199,6 @@ export class DnnColorPicker {
                 this.hslDisplay = "none";
                 this.hexDisplay = "none";
                 break;
-            default:
-                this.rgbDisplay = "flex";
-                this.hslDisplay = "none";
-                this.hexDisplay = "none";
             }
     }
 
@@ -261,7 +257,9 @@ export class DnnColorPicker {
                             />
                         </div>
                         <div class="dnn-color-mode-switch">
-                            <button id="rgb-switch" onClick={this.switchColorMode.bind(this)}>&#8597;</button>
+                            <button id="rgb-switch" onClick={this.switchColorMode.bind(this)}>
+                                <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="retweet" class="svg-inline--fa fa-retweet fa-w-20" role="img" viewBox="0 0 640 512"><path fill="currentColor" d="M629.657 343.598L528.971 444.284c-9.373 9.372-24.568 9.372-33.941 0L394.343 343.598c-9.373-9.373-9.373-24.569 0-33.941l10.823-10.823c9.562-9.562 25.133-9.34 34.419.492L480 342.118V160H292.451a24.005 24.005 0 0 1-16.971-7.029l-16-16C244.361 121.851 255.069 96 276.451 96H520c13.255 0 24 10.745 24 24v222.118l40.416-42.792c9.285-9.831 24.856-10.054 34.419-.492l10.823 10.823c9.372 9.372 9.372 24.569-.001 33.941zm-265.138 15.431A23.999 23.999 0 0 0 347.548 352H160V169.881l40.416 42.792c9.286 9.831 24.856 10.054 34.419.491l10.822-10.822c9.373-9.373 9.373-24.569 0-33.941L144.971 67.716c-9.373-9.373-24.569-9.373-33.941 0L10.343 168.402c-9.373 9.373-9.373 24.569 0 33.941l10.822 10.822c9.562 9.562 25.133 9.34 34.419-.491L96 169.881V392c0 13.255 10.745 24 24 24h243.549c21.382 0 32.09-25.851 16.971-40.971l-16.001-16z"/></svg>
+                            </button>
                         </div>
                     </div>
                     <div class="dnn-hsl-color-fields" style={{display: this.hslDisplay}}>
@@ -284,7 +282,9 @@ export class DnnColorPicker {
                             />
                         </div>
                         <div class="dnn-color-mode-switch">
-                            <button id="hsl-switch" onClick={this.switchColorMode.bind(this)}>&#8597;</button>
+                            <button id="hsl-switch" onClick={this.switchColorMode.bind(this)}>
+                                <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="retweet" class="svg-inline--fa fa-retweet fa-w-20" role="img" viewBox="0 0 640 512"><path fill="currentColor" d="M629.657 343.598L528.971 444.284c-9.373 9.372-24.568 9.372-33.941 0L394.343 343.598c-9.373-9.373-9.373-24.569 0-33.941l10.823-10.823c9.562-9.562 25.133-9.34 34.419.492L480 342.118V160H292.451a24.005 24.005 0 0 1-16.971-7.029l-16-16C244.361 121.851 255.069 96 276.451 96H520c13.255 0 24 10.745 24 24v222.118l40.416-42.792c9.285-9.831 24.856-10.054 34.419-.492l10.823 10.823c9.372 9.372 9.372 24.569-.001 33.941zm-265.138 15.431A23.999 23.999 0 0 0 347.548 352H160V169.881l40.416 42.792c9.286 9.831 24.856 10.054 34.419.491l10.822-10.822c9.373-9.373 9.373-24.569 0-33.941L144.971 67.716c-9.373-9.373-24.569-9.373-33.941 0L10.343 168.402c-9.373 9.373-9.373 24.569 0 33.941l10.822 10.822c9.562 9.562 25.133 9.34 34.419-.491L96 169.881V392c0 13.255 10.745 24 24 24h243.549c21.382 0 32.09-25.851 16.971-40.971l-16.001-16z"/></svg>
+                        </button>
                         </div>
                     </div>
                     <div class="dnn-hex-color-fields" style={{display: this.hexDisplay}}>
@@ -301,7 +301,9 @@ export class DnnColorPicker {
                             </div>
                         </div>
                         <div class="dnn-color-mode-switch">
-                            <button id="hex-switch" onClick={this.switchColorMode.bind(this)}>&#8597;</button>
+                            <button id="hex-switch" onClick={this.switchColorMode.bind(this)}>
+                                <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="retweet" class="svg-inline--fa fa-retweet fa-w-20" role="img" viewBox="0 0 640 512"><path fill="currentColor" d="M629.657 343.598L528.971 444.284c-9.373 9.372-24.568 9.372-33.941 0L394.343 343.598c-9.373-9.373-9.373-24.569 0-33.941l10.823-10.823c9.562-9.562 25.133-9.34 34.419.492L480 342.118V160H292.451a24.005 24.005 0 0 1-16.971-7.029l-16-16C244.361 121.851 255.069 96 276.451 96H520c13.255 0 24 10.745 24 24v222.118l40.416-42.792c9.285-9.831 24.856-10.054 34.419-.492l10.823 10.823c9.372 9.372 9.372 24.569-.001 33.941zm-265.138 15.431A23.999 23.999 0 0 0 347.548 352H160V169.881l40.416 42.792c9.286 9.831 24.856 10.054 34.419.491l10.822-10.822c9.373-9.373 9.373-24.569 0-33.941L144.971 67.716c-9.373-9.373-24.569-9.373-33.941 0L10.343 168.402c-9.373 9.373-9.373 24.569 0 33.941l10.822 10.822c9.562 9.562 25.133 9.34 34.419-.491L96 169.881V392c0 13.255 10.745 24 24 24h243.549c21.382 0 32.09-25.851 16.971-40.971l-16.001-16z"/></svg>
+                            </button>
                         </div>
                     </div>
                     
